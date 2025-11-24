cba2a5daae78aac2ab112c8d265ea880148ba14e
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-11-24 23:21:39 +0800

    chore(project): 初始化 Rust 项目结构
    - 添加 .gitignore 文件以忽略编译产物和 IDE 配置
    - 创建 Cargo.lock 和 Cargo.toml 文件用于依赖管理
    - 初始化 src/main.rs 文件并添加基础代码模板
    - 设置项目名称为 Rust-Data-Struct，版本号为 0.1.0
    - 指定 Rust edition 为 2024
    - 添加文件头注释模板和基础打印语句

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+/*
+ * @Author: MerlinSMQWQ MerlinSMQWQ@proton.me
+ * @Date: 2025-11-24 22:58:14
+ * @LastEditors: MerlinSMQWQ MerlinSMQWQ@proton.me
+ * @LastEditTime: 2025-11-24 23:20:48
+ * @FilePath: \Rust-Data-Struct\src\main.rs
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+fn main() {
+    println!("Hello, world!");
+}
