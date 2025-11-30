ec3a776d004aab15da1c3d56c5faf4484dfea66d
MerlinSMQWQ <MerlinSMQWQ@proton.me>
2025-11-30 23:10:59 +0800

    feat(double_linked_list): 实现双链表的插入和尾部添加功能
    - 添加 `push` 方法以支持在尾部添加元素
    - 添加 `insert` 方法以支持在指定位置插入元素
    - 优化 `push_front` 方法中的节点链接逻辑
    - 修复结构体初始化时的格式问题
    - 统一代码风格，改善可读性

--- src/linear_list/double_linked_list.rs
+++ src/linear_list/double_linked_list.rs
@@ -6,7 +6,7 @@ pub struct Node<T: std::fmt::Debug> {
     pub next: Option<NonNull<Node<T>>>,
 }
 
-impl <T: std::fmt::Debug> Node<T> {
+impl<T: std::fmt::Debug> Node<T> {
     pub fn new(element: T) -> Self {
         Self {
             pre: None,
@@ -18,7 +18,7 @@ impl <T: std::fmt::Debug> Node<T> {
 
 impl<T: std::fmt::Debug + Default> Default for Node<T> {
     fn default() -> Self {
-        Self{
+        Self {
             pre: None,
             data: T::default(),
             next: None,
@@ -32,12 +32,20 @@ pub struct DoubleLinkedList<T: std::fmt::Debug> {
     pub tail: Option<NonNull<Node<T>>>,
 }
 
-impl <T: std::fmt::Debug + Default> DoubleLinkedList<T> {
+impl<T: std::fmt::Debug + Default> DoubleLinkedList<T> {
     // 双链表的哨兵头结点
-    pub fn new() -> Self{
-        let head_node = Box::new(Node {pre: None, data: T::default(), next: None});
+    pub fn new() -> Self {
+        let head_node = Box::new(Node {
+            pre: None,
+            data: T::default(),
+            next: None,
+        });
         let head_ptr = NonNull::new(Box::into_raw(head_node));
-        Self { head: head_ptr, length: 0, tail: None }
+        Self {
+            head: head_ptr,
+            length: 0,
+            tail: None,
+        }
     }
 
     pub fn push_front(&mut self, element: T) {
@@ -46,16 +54,55 @@ impl <T: std::fmt::Debug + Default> DoubleLinkedList<T> {
         node.pre = self.head;
         node.next = head_node.next;
         let node_ptr = NonNull::new(Box::into_raw(node));
-        match head_node.next {
-            Some(mut next_ptr) => {
-                head_node.next = node_ptr;
-                (unsafe { next_ptr.as_mut().pre = node_ptr });
-            },
-            None => {
-                head_node.next = node_ptr;
-                self.tail = node_ptr;
-            },
+
+        if let Some(mut next_ptr) = head_node.next {
+            unsafe { next_ptr.as_mut().pre = node_ptr };
+        } else {
+            self.tail = node_ptr;
         }
+
+        head_node.next = node_ptr;
         self.length += 1;
+    }
+
+    pub fn push(&mut self, element: T) {
+        if self.length == 0 {
+            self.push_front(element);
+        } else {
+            let mut node = Box::new(Node::new(element));
+            node.pre = self.tail;
+            let tail_node = unsafe { self.tail.unwrap().as_mut() };
+            let node_ptr = NonNull::new(Box::into_raw(node));
+            tail_node.next = node_ptr;
+            self.tail = node_ptr;
+            self.length += 1;
+        }
+    }
+
+    pub fn insert(&mut self, element: T, pos: usize) -> Result<(), &'static str> {
+        if pos < 1 || pos > (self.length + 1) {
+            Err("Out of List!")
+        } else {
+            if pos == 1 {
+                self.push_front(element);
+            } else if pos == self.length + 1 {
+                self.push(element);
+            } else {
+                let mut node = Box::new(Node::new(element));
+                let mut current_node = unsafe { self.head.unwrap().as_mut() };
+                let mut current_ptr = self.head;
+                for _ in 1..(pos - 1) {
+                    current_ptr = current_node.next;
+                    current_node = unsafe { current_ptr.unwrap().as_mut() };
+                }
+                node.pre = current_ptr;
+                node.next = current_node.next;
+                let node_ptr = NonNull::new(Box::into_raw(node));
+                unsafe { current_node.next.unwrap().as_mut().pre = node_ptr };
+                current_node.next = node_ptr;
+                self.length += 1;
+            }
+            Ok(())
+        }
     }
 }
